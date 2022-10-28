2041981d2c7482d347e69501216637443bf6c39e
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2022-10-28 10:43:23 +0200

    Product class added

---
+++ DataLayer/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UnitPrice { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
--- DataLayer/NorthwindContext.cs
+++ DataLayer/NorthwindContext.cs
@@ -27,15 +27,24 @@ namespace DataLayer
         {
             base.OnModelCreating(modelBuilder);
 
+            //Category class
             modelBuilder.Entity<Category>().ToTable("categories");
             modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
             modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
             modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
 
-            //modelBuilder.Entity<Product>().ToTable("products");
-            //modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
-            //modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+            //Product class
+            modelBuilder.Entity<Product>().ToTable("products");
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+            //modelBuilder.Entity<Product>().Property(x => x.SupplierId).HasColumnName("supplierid");
             //modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+            modelBuilder.Entity<Product>().Property(x => x.QuantityPerUnit).HasColumnName("quantityperunit");
+            modelBuilder.Entity<Product>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+            modelBuilder.Entity<Product>().Property(x => x.UnitsInStock).HasColumnName("unitsinstock");
+
+
+
 
         }
     }
